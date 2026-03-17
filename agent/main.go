8f12630ff839151e3ef0af66c45fcc131e34b1ea
mosteligible <amtrijal147@gmail.com>
2026-03-16 22:01:19 -0400

    minimal grpc server

--- agent/main.go
+++ agent/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net"
+
+	"github.com/mosteligible/mcp-codemode/agent/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type Server struct {
+	pb.UnimplementedAgentServer
+}
+
+func (s *Server) Status(ctx context.Context, in *emptypb.Empty) (*pb.HealthStatus, error) {
+	return &pb.HealthStatus{
+		Code:    0,
+		Message: "healthy",
+	}, nil
+}
+
+func (s *Server) ExecuteCode(ctx context.Context, in *pb.ExecuteCodeRequest) (*pb.ExecuteCodeResponse, error) {
+	return &pb.ExecuteCodeResponse{
+		ExitCode: 0,
+		Output:   "Hello, World!\n",
+		Error:    "",
+	}, nil
+}
 
 func main() {
-	fmt.Println("Starting agent...")
+	listen, err := net.Listen("tcp", ":30031")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterAgentServer(grpcServer, &Server{})
+	slog.Info("Starting server on port: 30031")
+	if err := grpcServer.Serve(listen); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
