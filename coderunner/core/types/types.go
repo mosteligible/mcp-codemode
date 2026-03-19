f030e611e95b1ad881d48c88595f37bdaa009212
mosteligible <amtrijal147@gmail.com>
2026-03-18 23:26:08 -0400

    update types

--- coderunner/core/types/types.go
+++ coderunner/core/types/types.go
@@ -1,7 +1,5 @@
 package types
 
-import "net/http"
-
 type CodeRunnerRequest struct {
 	Code     string `json:"code"`
 	Language string `json:"language"`
@@ -11,11 +9,6 @@ type CommandOutput struct {
 	Output       string `json:"output"`
 	ErrorMessage string `json:"error,omitempty"`
 	Err          error  `json:"-"`
-}
-
-type CommResponse struct {
-	Response *http.Response
-	Err      error
 }
 
 type ProxyTarget struct {
