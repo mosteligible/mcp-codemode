1368c03e1fb71d29bec791e03dca2dd830f56b8c
mosteligible <amtrijal147@gmail.com>
2026-04-23 23:00:10 -0400

    user session to worker tracker with optional redis

--- coderunner/core/worker_client/worker_client.go
+++ coderunner/core/worker_client/worker_client.go
@@ -1,7 +1,11 @@
 package workerclient
 
 import (
+	"context"
+	"log/slog"
+
 	pb "github.com/mosteligible/mcp-codemode/agent-proto/pb"
+	"github.com/redis/go-redis/v9"
 	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -26,4 +30,58 @@ func NewWorkerClient(address string) (*WorkerClient, error) {
 		conn:   conn,
 		Client: client,
 	}, nil
+}
+
+type WorkerConnections struct {
+	Connections      map[string]*WorkerClient
+	sessionToHostMap map[string]string
+}
+
+func (wc *WorkerConnections) GetClientForHost(host string) (*WorkerClient, bool) {
+	client, ok := wc.Connections[host]
+	return client, ok
+}
+
+func (wc *WorkerConnections) CloseAll() {
+	for _, client := range wc.Connections {
+		client.conn.Close()
+	}
+}
+
+func (wc *WorkerConnections) AddClientForHost(host string, client *WorkerClient) {
+	wc.Connections[host] = client
+}
+
+func (wc *WorkerConnections) RemoveWorkerHost(ctx context.Context, host string, redisClient *redis.Client) {
+	delete(wc.Connections, host)
+	if redisClient == nil {
+		for sessionId, sessionhost := range wc.sessionToHostMap {
+			if host == sessionhost {
+				delete(wc.sessionToHostMap, sessionId)
+			}
+		}
+		return
+	}
+
+	redisClient.JSONDel(ctx, host, ".")
+}
+
+func (wc *WorkerConnections) RemoveUserSessionFromHost(ctx context.Context, host, sessionId string, redisClient *redis.Client) {
+	if redisClient == nil {
+		delete(wc.sessionToHostMap, sessionId)
+		return
+	}
+
+	redisClient.JSONDel(ctx, host, sessionId)
+}
+
+func (wc *WorkerConnections) AddUserSessionToHost(ctx context.Context, host, sessionId string, redisClient *redis.Client) error {
+	if redisClient == nil {
+		slog.Warn("using in-memory map for control planing of user session")
+		wc.sessionToHostMap[sessionId] = host
+		return nil
+	}
+
+	redisClient.JSONSet(ctx, host, sessionId, struct{}{})
+	return nil
 }
